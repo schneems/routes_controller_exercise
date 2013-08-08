64f3561658a2076b153378ee040d7072d8afc221
Kaylors <shuvophoenix@gmail.com>
2013-08-08 14:16:08 +0530

    Data

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ ControllerExercise::Application.routes.draw do
 
 
   get '/products' => 'products#index'
-
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
   resources :users
 
   # The priority is based upon order of creation:
